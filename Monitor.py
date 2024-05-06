e4e6e23ac828aa1d6b666178dcd9ee9fd9e99a4a
Piraldi <97887575+Piraldi@users.noreply.github.com>
2024-05-06 17:39:11 +0200

    Update Monitor.py

--- Monitor.py
+++ Monitor.py
@@ -20,10 +20,9 @@ def filter_dataframe_by_date(df):
     # Seleziona l'intervallo di date in una sidebar (container)
   with st.sidebar:
     st.subheader('Imposta filtro tra date:')
-    df['ConfermaNew']=df['Conferma'].str[:8]
-    df['Datanew'] = pd.to_datetime(df['ConfermaNew'], format='%d/%m/%y')
-    min_date = df['Datanew'].min().date()
-    max_date = df['Datanew'].max().date()
+    df['DATA PRELIEVO'] = pd.to_datetime(df['DATA PRELIEVO'], format='%d/%m/%y')
+    min_date = df['DATA PRELIEVO'].min().date()
+    max_date = df['DATA PRELIEVO'].max().date()
     with st.form('filtro'):
       # Crea un widget a scorrimento per selezionare le date
       start_date = st.date_input("Seleziona data inizio", min_date)
@@ -31,7 +30,7 @@ def filter_dataframe_by_date(df):
       submit = st.form_submit_button('Filtra')
       if submit:
        # Filtra il dataframe in base all'intervallo di date selezionato dall'utente
-       filtered_df = df[(df['Datanew'].dt.date >= start_date) & (df['Datanew'].dt.date <= end_date)]
+       filtered_df = df[(df['DATA PRELIEVO'].dt.date >= start_date) & (df['DATA PRELIEVO'].dt.date <= end_date)]
        return filtered_df 
     return df
      
@@ -49,31 +48,29 @@ def main():
         #st.write("Contenuto del file 'Missioni':")
         missioni_df = load_data(missioni)
         
-        # tengo le colonne dalla 2 alla 40
-        missioni_df = missioni_df.iloc[:, :41]
         st.write("Contenuto del file 'Missioni':")
         st.dataframe(missioni_df)
     
         # Rimuovere il punto come separatore delle migliaia e la virgola come separatore dei decimali
-        missioni_df['Quantità Movimentata'] = missioni_df['Quantità Movimentata'].str.replace('.', '').str.replace(',', '.')
+        missioni_df['QTA' PRELEVATA'] = missioni_df['QTA' PRELEVATA'].str.replace('.', '').str.replace(',', '.')
         
         # Tentare di convertire la colonna 'Quantità Movimentata' in tipo float
-        missioni_df['Quantità Movimentata'] = pd.to_numeric(missioni_df['Quantità Movimentata'], errors='coerce')
+        missioni_df['QTA' PRELEVATA'] = pd.to_numeric(missioni_df['QTA' PRELEVATA'], errors='coerce')
         
         # Filtrare le righe con NaN o valori non validi
-        righe_nan = missioni_df[missioni_df['Quantità Movimentata'].isna()]
+        righe_nan = missioni_df[missioni_df['QTA' PRELEVATA'].isna()]
 
         # Rimuovere le righe con NaN o valori non validi
-        missioni_df = missioni_df.dropna(subset=['Quantità Movimentata'])
+        missioni_df = missioni_df.dropna(subset=['QTA' PRELEVATA'])
 
         # Convertire la colonna 'Quantità Movimentata' in tipo int
-        missioni_df['Quantità Movimentata'] = missioni_df['Quantità Movimentata'].astype(int)
+        missioni_df['QTA' PRELEVATA'] = missioni_df['QTA' PRELEVATA'].astype(int)
         
         
         #filtro le missioni iin base alle date
         filtered_df = filter_dataframe_by_date(missioni_df)
-        #st.write("Database filtrato per data")
-        #st.dataframe(filtered_df)
+        st.write("Database filtrato per data")
+        st.dataframe(filtered_df)
 
 
 if __name__ == "__main__":
