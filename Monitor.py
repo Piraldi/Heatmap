f597e6190fd32b7972a38a8fa74f85de84635151
Piraldi <97887575+Piraldi@users.noreply.github.com>
2024-05-06 20:27:42 +0200

    Update Monitor.py

--- Monitor.py
+++ Monitor.py
@@ -21,6 +21,8 @@ def filter_dataframe_by_date(df):
   with st.sidebar:
     # Converti la colonna 'DATA PRELIEVO' in tipo datetime se non lo è già
     df['DATA PRELIEVO'] = pd.to_datetime(df['DATA PRELIEVO'], format='%d/%m/%Y')
+    # Converti la colonna 'ORARIO' in tipo datetime
+    df['ORARIO'] = pd.to_datetime(df['ORARIO'], format='%H:%M:%S')
     st.subheader('Imposta filtro tra date:')
     min_date = df['DATA PRELIEVO'].min().date()
     max_date = df['DATA PRELIEVO'].max().date()
@@ -34,7 +36,48 @@ def filter_dataframe_by_date(df):
        filtered_df = df[(df['DATA PRELIEVO'].dt.date >= start_date) & (df['DATA PRELIEVO'].dt.date <= end_date)]
        return filtered_df 
     return df
-     
+
+#funzione produttività per ordine
+def calculate_productivity_per_order(df):
+    
+    
+    # Conversione della colonna 'ORA PRELIEVO' in formato datetime
+    df['ORA PRELIEVO'] = pd.to_datetime(df['ORA PRELIEVO'], format='%H.%M.%S', errors='coerce')
+    
+    # Assicurati che 'Quantità Movimentata' sia in formato numerico
+    df['QTA PRELEVATA'] = pd.to_numeric(df['QTA PRELEVATA'], errors='coerce')
+    
+    # Rimuovi le righe con valori NaN nella colonna 'Quantità Movimentata'
+    df = df.dropna(subset=['QTA PRELEVATA'])
+
+    # Raggruppa per ordine, utente,data, tipo cliente e tipo prelievo
+    productivity_df_per_order = df.groupby(['ORDINE','UTENTE PRELIEVO', 'DATA PRELIEVO','TIPO SCATOLA']).agg({
+        'QTA PRELEVATA': 'sum',
+        'ARTICOLO': 'size',# Conteggio delle righe per ogni gruppo
+        'ORA PRELIEVO': lambda x: (x.max() - x.min()).total_seconds() / 3600,
+        }).reset_index()
+
+    # Calcola la produttività (quantità movimentata per ora)
+    productivity_df_per_order['Produttività'] = productivity_df_per_order['QTA PRELEVATA'] / productivity_df_per_order['ORA PRELIEVO']
+    # Rinomina le colonne
+    productivity_df_per_order.rename(columns={
+        'QTA PRELEVATA': 'Q tot',
+        'ARTICOLO': 'Righe tot',
+        'ORA PRELIEVO': 'Ore tot',
+        'Produttività': 'Produttività tot'
+    }, inplace=True)
+    
+    
+    return productivity_df_per_order
+
+def to_excel(df):
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, index=False)
+    processed_data = output.getvalue()
+    return processed_data
+
+
 # Funzione principale dell'applicazione Streamlit
 def main():
     st.title("Monitor Education")
@@ -67,6 +110,21 @@ def main():
         filtered_df = filter_dataframe_by_date(missioni_df)
         st.write("Database filtrato per data")
         st.dataframe(filtered_df)
+
+
+        # Chiama la funzione per calcolare la produttività per ordine
+        productivity_df_per_order = calculate_productivity_per_order(filtered_df)
+        st.header("Productivity Per Order")
+        # Crea una tabella interattiva per visualizzare i risultati
+        st.dataframe(productivity_df_per_order)
+        # Bottone di download
+        tab_1 = to_excel(productivity_df_per_order)
+        st.download_button(label='📥 Download tabella',
+                           data=tab_1,
+                           file_name='dataframe.xlsx',
+                           mime='application/vnd.ms-excel')
+        st.markdown("---")
+
 
 
 if __name__ == "__main__":
